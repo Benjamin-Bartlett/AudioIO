7d759dd65026f6eb4a0997b37532eb4a8a6ab1e1
tpicconatto <tpicconatto@potomacschool.org>
2022-12-17 12:36:23 -0500

    plots recorded sound onto a table

--- main.py
+++ main.py
@@ -1,37 +1,71 @@
 import pyaudio
 import numpy as np
+import wave
+import matplotlib.pyplot as plt
 import time
 
 p = pyaudio.PyAudio()
-
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
-CHUNK = 2**4
+CHUNK = 3200
 
-def callback(in_data, frame_count, time_info, flag):
+#def callback(in_data, frame_count, time_info, flag):
     # using Numpy to convert to array for processing
-    audio_data = np.frombuffer(in_data, dtype=np.float32)
-    x = 0
-    for a in audio_data:
-        a *= 1.1
+    #audio_data = np.frombuffer(in_data, dtype=np.float32)
+    #x = 0
+    #for a in audio_data:
+       # a *= 1.1
         #print("asdf")
-    print("------------------------ ")
-    return audio_data, pyaudio.paContinue
+    #print("------------------------ ")
+    #return audio_data, pyaudio.paContinue
 
 stream = p.open(format=pyaudio.paFloat32,
                 channels=CHANNELS,
                 rate=RATE,
                 output=True,
                 input=True,
-                frames_per_buffer=CHUNK,
-                stream_callback=callback)
+                frames_per_buffer=CHUNK,)
+                #stream_callback=callback)
+#testing
+seconds = 5
+#stream.start_stream()
+frames = []
+secondTack = 0
+secondCount = 0
+for i in range(0, int(RATE/CHUNK*seconds)):
+    data = stream.read(CHUNK)
+    frames.append(data)
 
-stream.start_stream()
-
-while stream.is_active():
-    time.sleep(20)
-    stream.stop_stream()
-    print("Stream is stopped")
-
+#while stream.is_active():
+    #time.sleep(20)
+    #stream.stop_stream()
+    #print("Stream is stopped")
+stream.stop_stream()
 stream.close()
 p.terminate()
+
+sFile = wave.open('recoding.wav','wb')
+sFile.setnchannels(CHANNELS)
+sFile.setsampwidth(p.get_sample_size(pyaudio.paFloat32))
+sFile.setframerate(RATE)
+sFile.writeframes(b''.join(frames))
+sFile.close()
+
+file = wave.open('recoding.wav','rb')
+sample_freq = file.getframerate()
+frames = file.getnframes()
+signal_wave = file.readframes(-1)
+file.close
+
+time = frames / sample_freq
+
+audio_array = np.frombuffer(signal_wave, dtype = np.float32)
+times = np.linspace(0, time, num=frames)
+
+plt.figure(figsize=(15,5))
+plt.plot(times,audio_array)
+plt.ylabel('Signal Wave')
+plt.xlabel('Time(s)')
+plt.xlim(0,time)
+plt.title('Sound')
+plt.show()
